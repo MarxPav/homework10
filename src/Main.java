e106fde46c9cf19829f01e4dd7e601419067f51a
Karaby <fablelite11@gmail.com>
2024-03-17 19:33:24 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+//Задача 1
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+//Задача 2
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+//Задача 3
+        fullName = fullName.replace("Ё", "Е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
         }
     }
